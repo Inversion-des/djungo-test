ae6b347994ca112870ef988d5cec18db5afcad9c
Inversion <yura.des@gmail.com>
2024-05-21 17:53:22 +0300

    article admin form and view works

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from .models import Article, ArticleImage, Category
 from .forms import ArticleImageForm
 
+
 class CategoryAdmin(admin.ModelAdmin):
 	list_display = ('category', 'slug')
 	prepopulated_fields = {'slug': ('category',)}
@@ -12,7 +13,6 @@ class CategoryAdmin(admin.ModelAdmin):
 			'fields': ('category', 'slug'),
 		}),
 	)
-
 
 admin.site.register(Category, CategoryAdmin)
 
@@ -29,26 +29,25 @@ class ArticleImageInline(admin.TabularInline):
 
 
 class ArticleAdmin(admin.ModelAdmin):
-	list_display = ('title', 'pub_date', 'slug', 'main_page')
-	inlines = [ArticleImageInline]
-	multiupload_form = True
-	multiupload_list = False
-	prepopulated_fields = {'slug': ('title',)}
-	raw_id_fields = ('category',)
+	# columns to display for the list of articles in the admin panel
+	list_display = ('title', 'pub_date', 'slug', 'main_page', 'category')
 	fieldsets = (
 		('', {
-			'fields': ('pub_date', 'title', 'description', 'main_page'),
+			'fields': ('pub_date', 'title', 'description', 'category', 'main_page'),
 		}),
-		((u'Додатково'), {
-			'classes': ('grp-collapse grp-closed',),
+		('Додатково', {
 			'fields': ('slug',),
 		}),
 	)
+	prepopulated_fields = {'slug': ('title',)}
+	inlines = [ArticleImageInline]
+	# raw_id_fields = ('category',)
+	# multiupload_form = True
+	# multiupload_list = False
 
 	def delete_file(self, pk, request):
 		'''Delete an image.'''
 		obj = get_object_or_404(ArticleImage, pk=pk)
 		return obj.delete()
-
 
 admin.site.register(Article, ArticleAdmin)
