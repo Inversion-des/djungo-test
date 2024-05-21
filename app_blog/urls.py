58d4478fe55bd077803e594d85f4105da0704558
Inversion <yura.des@gmail.com>
2024-05-21 17:54:20 +0300

    fixed: images are not loaded (404)

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from app_blog import views
 from .views import (HomePageView, ArticleDetail, ArticleList, ArticleCategoryList)
@@ -8,3 +10,6 @@ urlpatterns = [
 	path(r'articles/category/<slug>', ArticleCategoryList.as_view(), name='articles-category-list'),
 	path(r'articles/<year>/<month>/<day>/<slug>', ArticleDetail.as_view(), name='news-detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
